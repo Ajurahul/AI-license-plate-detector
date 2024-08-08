9dd1806fd1785b3284ff5e64f011c43764c0b186
Ajurahul <48558382+Ajurahul@users.noreply.github.com>
2024-08-08 21:04:45 +0530

    added licenseOCR

--- LicenseOCR.py
+++ LicenseOCR.py
@@ -1,5 +1,18 @@
+import re
+import requests
+from transformers import TrOCRProcessor , VisionEncoderDecoderModel
+from PIL import Image
+
 class LicenseOCR:
-    pass
+    def __int__(self):
+        print("Initialized")
+        self.processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-printed')
+        self.model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-printed')
+
+    def ocr_image(self, src_img):
+        pixel_values = self.processor(images=src_img, return_tensors="pt").pixel_values
+        generated_ids = self.model.generate(pixel_values)
+        return re.sub(r'[^a-zA-Z0-9]*', '', self.processor.batch_decode(generated_ids,skip_special_tokens=True)[0])
 
 
 def get_image():

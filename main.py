c2be2d8f555e473602f46f2f5706e81eddcf4401
Ajurahul <48558382+Ajurahul@users.noreply.github.com>
2024-08-08 21:14:42 +0530

    added main ccode

--- main.py
+++ main.py
@@ -26,7 +26,21 @@ def generate_pdf():
 
 
 def main():
-    pass
+    placeholder.write("Loadimg model. please wait....")
+    print("loading models")
+    ocr_processor = load_model()
+    db = UserDatabase('license_details.sqlite')
+    placeholder.write("Models loaded successfully")
+    uploaded_file = st.file_uploader("Choose an image file", type=['png', 'jpg', 'jpeg'])
+    if uploaded_file:
+        image_holder = st.image(uploaded_file, caption="uploaded Image", use_column_width=True, width=100)
+        placeholder.write("")
+        with st.spinner("Processing Image"):
+            src_img = get_image(uploaded_file)
+            ocr_text = ocr_processor.ocr_image(src_img)
+            print(ocr_text)
+            result = db.get_user_details(ocr_text)
+            print("Result"+result)
 
 
 if __name__ == "main":

35b911feedf838fe3fe2da4679a5c7286fdb5689
Ajurahul <48558382+Ajurahul@users.noreply.github.com>
2024-08-08 21:06:47 +0530

    complete ocr file

--- LicenseOCR.py
+++ LicenseOCR.py
@@ -1,7 +1,8 @@
 import re
 import requests
-from transformers import TrOCRProcessor , VisionEncoderDecoderModel
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 from PIL import Image
+
 
 class LicenseOCR:
     def __int__(self):
@@ -12,8 +13,14 @@ class LicenseOCR:
     def ocr_image(self, src_img):
         pixel_values = self.processor(images=src_img, return_tensors="pt").pixel_values
         generated_ids = self.model.generate(pixel_values)
-        return re.sub(r'[^a-zA-Z0-9]*', '', self.processor.batch_decode(generated_ids,skip_special_tokens=True)[0])
+        return re.sub(r'[^a-zA-Z0-9]*', '', self.processor.batch_decode(generated_ids, skip_special_tokens=True)[0])
 
 
-def get_image():
-    return None
+def get_image(path: str):
+    img = Image.open(path).convert('RGB')
+    return img
+
+
+def show_image(url):
+    img = Image.open(requests.get(url, stream=True).raw).convert('RGB')
+    return img

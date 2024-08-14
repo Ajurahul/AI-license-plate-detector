d9ae20fe11e449f2567999877ccf97bcbae9e7eb
Ajurahul <48558382+Ajurahul@users.noreply.github.com>
2024-08-14 11:49:37 +0530

    Update LicenseOCR.py

--- LicenseOCR.py
+++ LicenseOCR.py
@@ -1,11 +1,23 @@
 import re
+
 import requests
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 from PIL import Image
 
 
+def get_image(path):
+    img = Image.open(path).convert("RGB")
+
+    return img
+
+
+def show_image(url):
+    img = Image.open(requests.get(url, stream=True).raw).convert("RGB")
+    return img
+
+
 class LicenseOCR:
-    def __int__(self):
+    def __init__(self):
         print("Initialized")
         self.processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-printed')
         self.model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-printed')
@@ -14,13 +26,3 @@ class LicenseOCR:
         pixel_values = self.processor(images=src_img, return_tensors="pt").pixel_values
         generated_ids = self.model.generate(pixel_values)
         return re.sub(r'[^a-zA-Z0-9]*', '', self.processor.batch_decode(generated_ids, skip_special_tokens=True)[0])
-
-
-def get_image(path: str):
-    img = Image.open(path).convert('RGB')
-    return img
-
-
-def show_image(url):
-    img = Image.open(requests.get(url, stream=True).raw).convert('RGB')
-    return img
